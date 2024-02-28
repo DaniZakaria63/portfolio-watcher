f08918e254a4394f24b04b61713ddd43e609be1a
DaniZakaria63 <daniootsutsuki@gmail.com>
2024-02-28 13:38:14 +0700

    set the java version

--- model/build.gradle.kts
+++ model/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.JavaVersion
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.androidLibrary)
@@ -29,7 +31,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 }
 
