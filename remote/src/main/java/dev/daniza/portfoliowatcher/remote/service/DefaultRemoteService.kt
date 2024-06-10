df9125447681c060822c77c507a8398d364c1f1b
DaniZakaria63 <daniootsutsuki@gmail.com>
2024-06-10 16:21:36 +0700

    call the default network request with the unit test

--- remote/src/main/java/dev/daniza/portfoliowatcher/remote/service/DefaultRemoteService.kt
+++ remote/src/main/java/dev/daniza/portfoliowatcher/remote/service/DefaultRemoteService.kt
@@ -1,10 +1,14 @@
 package dev.daniza.portfoliowatcher.remote.service
 
+import com.google.gson.JsonObject
 import dev.daniza.portfoliowatcher.remote.endpoint.RemoteEndpoint
+import retrofit2.Call
 import javax.inject.Inject
 
 class DefaultRemoteService @Inject constructor(
-    val remoteEndpoint: RemoteEndpoint
+    private val remoteEndpoint: RemoteEndpoint
 ) : RemoteService {
-
+    override suspend fun getCoins(): JsonObject {
+        return remoteEndpoint.getCoins()
+    }
 }
